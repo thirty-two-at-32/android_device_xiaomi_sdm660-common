f4fe98049c584c904834bc6ada2d52413bedb8a8
Vivekachooz <vivekachooz@gmail.com>
2022-03-10 10:40:12 +0530

    sdm660-common: Update GPS hal from LA.UM.9.2.r1-03700-SDMxx0.0
    Change-Id: I3fdc67c2b5edc74357963b418fb93ac9cd5820e3

--- sdm660.mk
+++ sdm660.mk
@@ -226,14 +226,15 @@ PRODUCT_PACKAGES += \
     libgnss \
     libsensorndkbridge \
     libwifi-hal-ctrl
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    
+PRODUCT_PACKAGES += \
+    flp.conf \
+    gnss_antenna_info.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
 
 # Health
 PRODUCT_PACKAGES += \
