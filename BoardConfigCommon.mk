1b0d4254ccb7c7f9102f2739002ebfed188ce51e
kyasu <kyasu.dev@gmail.com>
2022-03-23 11:44:30 +0900

    sdm660-common: Remove multisim.config if device is a single SIM model
    * clover is a single SIM model.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,6 +136,9 @@ TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+ifeq ($(filter clover,$(TARGET_DEVICE)),)
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor-dsds.prop
+endif
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
