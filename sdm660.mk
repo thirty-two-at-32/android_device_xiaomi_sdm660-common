693c306836c5b3861dbbda79d04244d3ac9d0c6f
Alcatraz323 <alcatraz32323@gmail.com>
2023-03-03 15:10:50 +0800

    sdm660-common: power: use more advanced cgroup/task profile
    * CONFIG_CGROUP_FREEZER is enabled
    * CONFIG_UCLAMP_TASK is backported and enabled
    
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -136,8 +136,8 @@ endif
 
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
