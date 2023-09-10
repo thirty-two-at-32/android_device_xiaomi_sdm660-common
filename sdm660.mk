2422fe90c85ff5bb85886480a21cb8291dc45568
madmax7896 <madmax78968@gmail.com>
2023-09-10 10:16:28 -0300

    sdm660-common: Import Dolby audio blobs and sepolicy
    Squashed with:
    commit b1baf8e4109edf71727c39bff18077110174dff2
    Author: Yuan Si <do4suki@gmail.com>
    Date:   Sat Sep 16 06:35:46 2023 +0800
    
        sweet: sepolicy: Address more Dolby audio denials
    
        09-16 06:33:17.209   613   613 E SELinux : avc:  denied  { find } for interface=vendor.dolby.hardware.dms::IDms sid=u:r:mediacodec:s0 pid=1262 scontext=u:r:mediacodec:s0 tcontext=u:object_r:hal_dms_hwservice:s0 tclass=hwservice_manager permissive=0
        09-16 06:33:17.209  1262  8489 E DecDynamicConfig: connectService : Connect to Dolby Native Service failed
        09-16 06:33:17.209  1262  8489 E DecDynamicConfig: checkShareConfig: Dolby service is NULL
    
        Change-Id: Ideb557073b53e7b31f6535d6053933104b455ae7
        Signed-off-by: Yuan Si <do4suki@gmail.com>
    
    commit e83b80995343c56382e43bf0656ee1e2b79ee067
    Author: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Date:   Thu Oct 12 00:02:27 2023 +0800
    
        sweet: sepolicy: Address mediacodec denials
    
        Partially taken from https://github.com/Nabu-upsidedowncake/device_xiaomi_marble/commit/ec0e9c641f40d21cdd8e7f6783903208a5005f1f and extended to hal_dms_server
    
        Change-Id: I51cfbbb2f8b9d83e74ef2f44196e8393189ca3dc
        Signed-off-by: basamaryan <basam.aryan@gmail.com>
    
    commit 0a7aab81667f137c57fa95c4661fa7e43e0e2c1c
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Fri Aug 2 20:27:52 2024 +0530
    
        sweet: Move dax-default to device tree
    
        Change-Id: Id2054bb5e61bcfe58706e32f44d37fe7524aa912
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I2d466238525d074269377a82627a816527aefd74
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -91,6 +91,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
+    $(COMMON_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
     $(COMMON_PATH)/configs/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(COMMON_PATH)/configs/audio/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
     $(COMMON_PATH)/configs/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
