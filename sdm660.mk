76e8e5022273c86cdb3abc216e6d059804be11be
Nikita Ioffe <ioffe@google.com>
2023-11-03 15:00:07 +0800

    sdm660-common: device: Inherit userspace_reboot.mk
    Test: Checked that userspace_reboot.mk is included in device makefiles
    Bug: 135984674
    Change-Id: If910e2a6baf8acaafc8bafdf8193ba7237f66f1a
    Merged-In: If910e2a6baf8acaafc8bafdf8193ba7237f66f1a

--- sdm660.mk
+++ sdm660.mk
@@ -23,6 +23,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
 
+# Enable userspace reboot
+$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
