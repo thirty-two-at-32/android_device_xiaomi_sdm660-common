977f6d7aea1c6c4df0e7b04382b17e30aefbaeb0
GuaiYiHu <guaiyihu@foxmail.com>
2024-02-27 18:49:21 +0800

    sdm660-common: keylayout: Map Button Jack keylayout to fix headset button controls
    * input: sdm660-snd-card Button Jack as
    /devices/soc/soc:sound/sound/card0/input8

--- sdm660.mk
+++ sdm660.mk
@@ -268,6 +268,7 @@ OVERRIDE_ENABLE_UFFD_GC := false
 
 # Keylayout
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
