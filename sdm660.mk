22c9757fe936cfe66eb2585f80b28a742b89cd8d
LuK1337 <priv.luk@gmail.com>
2024-11-02 12:45:07 +0100

    sdm660-common: Build libhidlbase-v32{,.vendor}
    Change-Id: I1b31199ec389ea6ba0d8faea4c539839c1cb8170

--- sdm660.mk
+++ sdm660.mk
@@ -253,7 +253,9 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    libhidlbase-v32 \
+    libhidlbase-v32.vendor
 
 # IDC
 PRODUCT_COPY_FILES += \
