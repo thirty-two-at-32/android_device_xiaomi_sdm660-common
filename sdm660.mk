1afa6016152e7ca15521ffb30bb990d666a2774f
Aeoniixx <aeoniixx@gmail.com>
2025-06-13 18:26:28 +0530

    sdm660-common: build android.frameworks.sensorservice@1.0
    init: Control message: Could not find 'aidl/android.frameworks.sensorservice.ISensorManager/default' for ctl.interface_start from pid: 535 (/system/bin/servicemanager)
    
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -467,6 +467,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     libpower.vendor
 
