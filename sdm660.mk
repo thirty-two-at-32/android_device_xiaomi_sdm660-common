b2d0ddecc36160f948ef625bc82b3189d1934fa4
pix106 <sbordenave@gmail.com>
2025-06-16 19:55:48 +0200

    sdm660-common: build libaudioclient_shim

--- sdm660.mk
+++ sdm660.mk
@@ -75,6 +75,7 @@ PRODUCT_PACKAGES += \
     audio.usbv2.default \
     liba2dpoffload \
     libaudio-resampler \
+    libaudioclient_shim \
     libaudioroute.vendor \
     libhdmiedid \
     libhfp \
