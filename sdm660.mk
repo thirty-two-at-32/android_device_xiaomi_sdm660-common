57ac3e58fb03c13d9c7aa68ab4248fff0bec0546
sabarop <sabar.pakpahan@gmail.com>
2025-07-07 10:47:46 +0700

    sdm660-common: Do not use the fragment for QTI vibrator HAL
    * The reverted commit was including the whole fragment for vibrator.
    * In this folder, the flag BOARD_OPENSOURCE_DIR was set, which used
    to break audio building by setting a bad path for our source, instead
    of hardware/qcom-caf/sdm660/audio
    
    Co-Authored-By: Onelots <54822802+onelots@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -707,7 +707,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
 # Vibrator
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_PACKAGES += \
