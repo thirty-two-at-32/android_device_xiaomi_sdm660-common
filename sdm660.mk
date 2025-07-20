fae4efa9886fdcb4696a75527af0ed32e40a8a3a
sabarop <sabar.pakpahan@gmail.com>
2025-07-20 10:26:16 +0700

    sdm660-common: Import Dolby Atmos
    from https://github.com/userariii/vendor_sony_dolby

--- sdm660.mk
+++ sdm660.mk
@@ -23,9 +23,6 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
-#Dolby
-$(call inherit-product, vendor/sony/dolby/setup.mk)
-
 # Common Tree Path
 COMMON_PATH := device/xiaomi/sdm660-common
 
@@ -460,6 +457,7 @@ PRODUCT_PACKAGES += \
     libstagefright_bufferqueue_helper.vendor \
     libstagefright_enc_common.vendor \
     libstagefright_flacdec.vendor \
+    libstagefright_foundation-v33 \
     libstagefright_foundation.vendor \
     libstagefright_omx.vendor \
     libstagefright_omx_utils.vendor \
