b1d4dca24c3e4fea05abd7a61f50c4674b21b369
sabarop <sabar.pakpahan@gmail.com>
2025-08-16 23:13:59 +0700

    Revert "sdm660-common: Import Dolby Atmos"
    This reverts commit fae4efa9886fdcb4696a75527af0ed32e40a8a3a.

--- sdm660.mk
+++ sdm660.mk
@@ -23,6 +23,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
+#Dolby
+$(call inherit-product, vendor/sony/dolby/setup.mk)
+
 # Common Tree Path
 COMMON_PATH := device/xiaomi/sdm660-common
 
@@ -461,7 +464,6 @@ PRODUCT_PACKAGES += \
     libstagefright_bufferqueue_helper.vendor \
     libstagefright_enc_common.vendor \
     libstagefright_flacdec.vendor \
-    libstagefright_foundation-v33 \
     libstagefright_foundation.vendor \
     libstagefright_omx.vendor \
     libstagefright_omx_utils.vendor \
