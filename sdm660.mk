76fe1689a24a5e74f468e8e6c44b2fb973688531
sabarop <sabar.pakpahan@gmail.com>
2025-08-24 20:59:00 +0700

    sdm660-common: Update boot-image-profile.txt path for A15 QPR2

--- sdm660.mk
+++ sdm660.mk
@@ -54,7 +54,7 @@ PRODUCT_COPY_FILES += \
 
 # Android GO
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # ANT+
 PRODUCT_PACKAGES += \
