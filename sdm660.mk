73516dd6334316e928bc4b29e8fbaddb71359b7d
sabarop <sabar.pakpahan@gmail.com>
2025-08-26 11:25:55 +0700

    Revert "sdm660-common: Downgrade graphics blobs from LA.UM.8.2.1.r1-05600-sdm660"
    This reverts commit 4dc9cf7fc080b2ea3dcae0fa3139b0561244072f.

--- sdm660.mk
+++ sdm660.mk
@@ -155,9 +155,9 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl:64 \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
     gralloc.sdm660
 
 PRODUCT_PACKAGES += \
@@ -174,11 +174,13 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.frameworks.displayservice@1.0 \
     libdisplayconfig \
+    libgralloc.qti \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.9 \
-    vendor.display.config@2.0
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
