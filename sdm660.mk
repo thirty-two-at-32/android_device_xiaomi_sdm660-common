81645740a6c5200f75500ad2a1e317188f8191c2
sabarop <sabar.pakpahan@gmail.com>
2025-08-28 20:44:01 +0700

    sdm660-common: device: Fix build Lineage Health HAL

--- sdm660.mk
+++ sdm660.mk
@@ -320,11 +320,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
