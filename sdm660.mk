1be4321e17fd6e6f959595e4458510874b844336
sabarop <sabar.pakpahan@gmail.com>
2025-09-02 08:28:47 +0700

    sdm660-common: Update perf blobs from S62Pro

--- sdm660.mk
+++ sdm660.mk
@@ -306,7 +306,21 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.0 \
+    android.hardware.power@1.0.vendor \
+    android.hardware.power@1.1 \
+    android.hardware.power@1.1.vendor \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.2.vendor \
+    android.hardware.power@1.3 \
+    android.hardware.power@1.3.vendor \
     android.hardware.power-service-qti
+
+# Perf
+PRODUCT_PACKAGES += \
+   libtflite \
+   libtextclassifier_hash \
+   vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
